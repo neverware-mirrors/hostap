f1c4d2bf74ede7a0b4bed1cfefd11a0a280404cc
Paul Stewart <pstew@chromium.org>
2016-01-19 17:48:02 -0800

    wpa_supplicant; nl80211: Enable critical protocol
    For some network drivers, the "low initial bitrates"
    implementation in wpa_supplicant is not sufficient to prevent
    the rate controller from sending with high bitrates or
    initiating background scans during EAPOL and DHCP.  This CL
    employs the nl80211 ciritcal protocol feature to engage
    drivers that support this feature in temporarily communicating
    in a more reliable fashion.
    
    BUG=chrome-os-partner:37871
    TEST=emerge-$BOARD wpa_supplicant + network_WiFi_LowInitialBitrates.
    Note that "Failed to {enable|disable} high rates" messages no longer appear.
    
    Change-Id: I78c6897b16dc91c5346a411ea235babf5af0ad49
    Original-Reviewed-on: https://chromium-review.googlesource.com/324052
    Original-Commit-Ready: Eric Caruso <ejcaruso@chromium.org>
    Original-Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Original-Reviewed-by: mukesh agrawal <quiche@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/669520
    Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Kevin Cernekee <cernekee@chromium.org>

--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -1288,6 +1288,8 @@ struct wpa_driver_capa {
 #define WPA_DRIVER_FLAGS_FULL_AP_CLIENT_STATE	0x0000010000000000ULL
 /** Driver supports P2P Listen offload */
 #define WPA_DRIVER_FLAGS_P2P_LISTEN_OFFLOAD     0x0000020000000000ULL
+/* Driver supports critical protocol */
+#define WPA_DRIVER_FLAGS_CRIT_PROTOCOL		0x0000040000000000ULL
 	u64 flags;
 
 #define FULL_AP_CLIENT_STATE_SUPP(drv_flags) \
