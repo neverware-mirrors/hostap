a43e3f9b0d2feba0d86fea9c947e6f11bcef51ad
Zhonglin Zhang <zhonglin@codeaurora.org>
2019-04-04 15:56:02 +0800

    UPSTREAM: WNM: Fetch scan results before checking transition candidates
    On receiving a WNM BSS Transition Management Request frame with a
    candidate list, fetch the latest scan results from the kernel to see if
    there are any recent scan results for the candidates and initiate a
    connection if found. This helps to avoid triggering a new scan in cases
    where a scan initiated by something else (e.g., an internal beacon
    measurement report functionality in a driver) has processed Beacon or
    Probe Response frames without wpa_supplicant having received a
    notification of such an update yet.
    
    Change-Id: Ib9f5cdbd80f2d749c3c0f06ac6affd8f5993acbf
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    (Applied from commit 2f195639ec9a568d0758c41d39f3358d3efbcbbf)
    (Source: git://w1.fi/srv/git/hostap.git)
    
    Signed-off-by: Zhonglin Zhang <zhonglin@codeaurora.org>
    Signed-off-by: Vikas Patel <vikpatel@codeaurora.org>
    Change-Id: I6b4d9fb4b17296ef61f6f124241cfb3938d12704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/1605534
    Reviewed-by: Ratheesh Selvakumar Pappa <ratheeshs@google.com>
    Reviewed-by: Julan Hsu <julanhsu@google.com>
    Commit-Queue: Julan Hsu <julanhsu@google.com>
    Tested-by: Julan Hsu <julanhsu@google.com>

--- wpa_supplicant/scan.h
+++ wpa_supplicant/scan.h
@@ -55,5 +55,10 @@ void wpas_mac_addr_rand_scan_clear(struct wpa_supplicant *wpa_s,
 int wpas_mac_addr_rand_scan_set(struct wpa_supplicant *wpa_s,
 				unsigned int type, const u8 *addr,
 				const u8 *mask);
+void filter_scan_res(struct wpa_supplicant *wpa_s,
+		     struct wpa_scan_results *res);
+void scan_snr(struct wpa_scan_res *res);
+void scan_est_throughput(struct wpa_supplicant *wpa_s,
+			 struct wpa_scan_res *res);
 
 #endif /* SCAN_H */
